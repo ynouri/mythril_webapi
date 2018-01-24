7576f43b3cd78be8fedb0eaf14ef15a3c99a49d8
ynouri <ynouri@users.noreply.github.com>
2018-01-23 20:11:56 -0500

    Remove myth absolute path

--- analysis/tasks.py
+++ analysis/tasks.py
@@ -4,9 +4,10 @@ import subprocess
 
 @shared_task
 def myth_task(bytecode):
-	mythril_dir = "/Users/ynouri/Dropbox/Projects/Topcoder-Mythril/mythril/"
+	#mythril_dir = "[Insert your mythril path here]/mythril/"<-- this shouldn't be needed, removed for Heroku deployment
 	args = ['myth', '-x', '-c', bytecode]
-	p = subprocess.Popen(args, cwd=mythril_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	#p = subprocess.Popen(args, cwd=mythril_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	stdout = p.stdout.read().decode('utf-8')
 	stderr = p.stderr.read().decode('utf-8')
 	task_result = {'stdout': stdout, 'stderr': stderr}
